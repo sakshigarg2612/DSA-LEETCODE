648880289c4a12d9595ecadba83d3f5f24e10f6e
Sakshi <43674855+sakshigarg2612@users.noreply.github.com>
2025-05-03 12:57:00 +0530

    Time: 2 ms (100.00%) | Memory: 49.9 MB (91.20%) - LeetSync

--- 1049-minimum-domino-rotations-for-equal-row/minimum-domino-rotations-for-equal-row.java
+++ 1049-minimum-domino-rotations-for-equal-row/minimum-domino-rotations-for-equal-row.java
@@ -1,57 +1,26 @@
 class Solution {
     public int minDominoRotations(int[] tops, int[] bottoms) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        HashMap<Integer, Integer> bottomMap = new HashMap<>();
-        for (int ele : tops) {
-            map.put(ele, map.getOrDefault(ele, 0) + 1);
+        for (int i = 1; i <= 6; i++) {
+            int rotations = check(i, tops, bottoms);
+            if (rotations != -1)
+                return rotations;
         }
-        for (int ele : bottoms) {
-            bottomMap.put(ele, bottomMap.getOrDefault(ele, 0) + 1);
-        }
-        int maxTop = 0;
-        int topKey = 0;
-        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-            if (maxTop < entry.getValue()) {
-                maxTop = entry.getValue();
-                topKey = entry.getKey();
-            }
-        }
-        int maxBottom = 0;
-        int bottomKey = 0;
-        for (Map.Entry<Integer, Integer> entry : bottomMap.entrySet()) {
-            if (maxBottom < entry.getValue()) {
-                maxBottom = entry.getValue();
-                bottomKey = entry.getKey();
-            }
-        }
-        int countTop = 0;
-        int topNew[] = Arrays.copyOf(tops, tops.length);
-        for (int i = 0; i < tops.length; i++) {
-            if (topNew[i] == topKey) {
-                continue;
-            }
-            if (tops[i] != topKey && bottoms[i] == topKey) {
-                topNew[i] = topKey;
-                countTop++;
-            }
-        }
+        return -1;
+    }
 
-        if (!Arrays.stream(topNew).allMatch(x -> x == topNew[0])) {
-            countTop = Integer.MAX_VALUE;
-        }
-        int countBottom = 0;
-        for (int i = 0; i < bottoms.length; i++) {
-            if (bottoms[i] != bottomKey && tops[i] == bottomKey) {
-                countBottom++;
-                bottoms[i] = bottomKey;
+    int check(int target, int[] tops, int[] bottoms) {
+        int top = 0;
+        int bottom = 0;
+        for (int i = 0; i < tops.length; i++) {
+            if (tops[i] != target && bottoms[i] != target) {
+                return -1;
+            } else if (tops[i] != target) {
+                top++;
+            } else if(bottoms[i]!=target) {
+                bottom++;
             }
         }
-        if (Arrays.stream(bottoms).allMatch(x -> x == bottoms[0])) {
-            return Math.min(countTop, countBottom);
-        }
-        if (countTop > tops.length) {
-            return -1;
-        }
-        return countTop;
+         return Math.min(top, bottom);
+
     }
 }
