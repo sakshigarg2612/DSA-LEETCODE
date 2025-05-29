9a67764c33bfd3709d4182ac8f38f3e3e3d18952
Sakshi <43674855+sakshigarg2612@users.noreply.github.com>
2025-05-29 21:02:49 +0530

    Time: 20 ms (76.09%) | Memory: 51.5 MB (10.02%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -3,28 +3,22 @@ class Solution {
         int m = text1.length();
         int n = text2.length();
         int[][] dp = new int[m][n];
-        for (int[] row : dp) {
-            Arrays.fill(row, -1);
+        dp[0][0] = text1.charAt(0) == text2.charAt(0) ? 1 : 0;
+        for(int i=1; i<m ;i++) {
+            dp[i][0] = text1.charAt(i) == text2.charAt(0) ? 1 : dp[i-1][0];
+        } 
+        for(int i=1; i<n ;i++) {
+            dp[0][i] = text1.charAt(0) == text2.charAt(i) ? 1 :dp[0][i-1];
         }
-        return helper(m - 1, n - 1, text1, text2, dp);
-    }
-
-    int helper(int i, int j, String text1, String text2,int[][]dp) {
-        if(i <0 || j<0) {
-            return 0;
+        for(int i =1; i <m;i++) {
+            for(int j=1; j <n;j++) {
+                if(text1.charAt(i) == text2.charAt(j)) {
+                    dp[i][j] = dp[i-1][j-1] +1;
+                    continue;
+                }
+                dp[i][j] = Math.max(dp[i-1][j] , dp[i][j-1]);
+            }
         }
-        if(dp[i][j]!=-1) return dp[i][j];
-        if (text1.charAt(i) == text2.charAt(j)) {
-            return helper(i - 1, j - 1, text1, text2,dp) + 1;
-        }
-        // if (i == 0 && j != 0) {
-        //     return helper(i, j - 1, text1, text2,dp);
-        // }
-        // if (i != 0 && j == 0) {
-        //     return helper(i-1, j, text1, text2,dp);
-        // }
-        int move1 = helper(i - 1, j, text1, text2,dp);
-        int move2 = helper(i, j - 1, text1, text2,dp);
-        return dp[i][j] = Math.max(move1, move2);
+        return dp[m - 1][n - 1];
     }
 }
