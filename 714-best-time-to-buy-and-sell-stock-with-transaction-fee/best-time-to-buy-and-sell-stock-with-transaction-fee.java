1804d68692293f154cb1aeeb608f8808c5a7eb01
Sakshi <43674855+sakshigarg2612@users.noreply.github.com>
2025-07-15 23:25:32 +0530

    Time: 5 ms (76.53%) | Memory: 55.1 MB (71.35%) - LeetSync

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.java
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.java
@@ -1,24 +1,26 @@
 class Solution {
     public int maxProfit(int[] prices, int fee) {
         int n = prices.length;
+        
+        // Future states
+        int aheadBuy = 0;
+        int aheadSell = 0;
 
-        // Extra padding to safely access index + 2
-        int[][] dp = new int[n + 2][2];
-
-        for (int index = n - 1; index >= 0; index--) {
-            // Buy state: either buy now (-price) or skip
-            dp[index][0] = Math.max(
-                -prices[index] + dp[index + 1][1]-fee,  // buy
-                dp[index + 1][0]                    // skip
+        for (int i = n - 1; i >= 0; i--) {
+            int currBuy = Math.max(
+                -prices[i] - fee + aheadSell,  // Buy and pay fee
+                aheadBuy                       // Skip
             );
 
-            // Sell state: either sell now (+price) or skip
-            dp[index][1] = Math.max(
-                prices[index] + dp[index + 1][0],   // sell + cooldown
-                dp[index + 1][1]                    // skip
+            int currSell = Math.max(
+                prices[i] + aheadBuy,          // Sell
+                aheadSell                      // Skip
             );
+
+            aheadBuy = currBuy;
+            aheadSell = currSell;
         }
 
-        return dp[0][0];  // start at day 0 with buy allowed
+        return aheadBuy;  // Start at day 0 with option to buy
     }
 }
